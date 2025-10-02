24a571af29e9f4dd4cd339a8350b0e12e978587a
niyooshawebsites <niyooshawebsites@gmail.com>
2025-10-02 13:22:13 +0530

    error fix code

--- models/user.model.js
+++ models/user.model.js
@@ -2,31 +2,35 @@ import mongoose from "mongoose";
 
 // schema
 
-const userSchema = new mongoose.Schema({
-    email:{
-        type : String,
-        unique: [true,"Email must be unique"],
-        require: [true,"email is required"],
-        trim: true,
-        match:[/^\S+@\S+\.\S+$/,"Please enter valid email"],
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      unique: [true, "Email must be unique"],
+      require: [true, "email is required"],
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Please enter valid email"],
     },
-    password:{
-        type:String,
-        required : true,
-        min:[8,"password is less than 8 character"],
-        max: [32,"password is more than 20 characters"],
-        match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?]).{8,32}$/,"password is not valid"],
+    password: {
+      type: String,
+      required: true,
+      min: [8, "password is less than 8 character"],
+      max: [32, "password is more than 20 characters"],
+      match: [
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/,
+        "password is not valid",
+      ],
     },
-    role:{
-        type: String,
-        required: true,
-        enum:["admin","user"],
-        default:"user",
-    }
+    role: {
+      type: String,
+      required: true,
+      enum: ["admin", "user"],
+      default: "user",
     },
-    {
-    timestamps:true,
-    }
+  },
+  {
+    timestamps: true,
+  }
 );
 
-export default mongoose.model("User",userSchema);
+export default mongoose.model("User", userSchema);
